182259c1a4dc586ccda46377a3bd56e48b708116
Mahbubur Choudhury <mahbubc2012@gmail.com>
2020-04-08 18:27:46 -0400

    social lab

---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { post } from './post';
+import { SocialPosts } from './social-posts';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css'],
+})
+export class SocialPostsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
